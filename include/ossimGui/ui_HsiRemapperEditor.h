de20f47236e0fcece5b5d4fb653c2e153a315669
Garrett Potts <gpotts@radiantblue.com>
2016-01-04 09:56:03 -0500

    Removed Qt3 include

--- include/ossimGui/ui_HsiRemapperEditor.h
+++ include/ossimGui/ui_HsiRemapperEditor.h
@@ -9,7 +9,6 @@
 #ifndef UI_HSIREMAPPEREDITOR_H
 #define UI_HSIREMAPPEREDITOR_H
 
-#include <Qt3Support/Q3MimeSourceFactory>
 #include <QtCore/QVariant>
 #include <QtGui/QAction>
 #include <QtGui/QApplication>
