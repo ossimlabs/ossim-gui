0c60729c28f4f15cb80a68176993d30760a925a6
garrett.potts <garrett.potts@vantor.com>
2026-07-16 12:03:44 -0400

    Modified the find

--- cmake/Findossim-registration-source.cmake
+++ cmake/Findossim-registration-source.cmake
@@ -19,12 +19,6 @@ if(TARGET ossim-registration-source)
 endif()
 
 set(_OSSIM_REGISTRATION_SOURCE_HINTS)
-if(DEFINED OSSIM_REGISTRATION_SOURCE_ROOT)
-  list(APPEND _OSSIM_REGISTRATION_SOURCE_HINTS "${OSSIM_REGISTRATION_SOURCE_ROOT}")
-endif()
-if(DEFINED ENV{OSSIM_REGISTRATION_SOURCE_ROOT})
-  list(APPEND _OSSIM_REGISTRATION_SOURCE_HINTS "$ENV{OSSIM_REGISTRATION_SOURCE_ROOT}")
-endif()
 if(DEFINED OSSIM_AUTOREG_ROOT)
   list(APPEND _OSSIM_REGISTRATION_SOURCE_HINTS "${OSSIM_AUTOREG_ROOT}")
 endif()
@@ -32,8 +26,24 @@ if(DEFINED ENV{OSSIM_AUTOREG_ROOT})
   list(APPEND _OSSIM_REGISTRATION_SOURCE_HINTS "$ENV{OSSIM_AUTOREG_ROOT}")
 endif()
 list(APPEND _OSSIM_REGISTRATION_SOURCE_HINTS
-  "${CMAKE_CURRENT_LIST_DIR}/../../ossim-autoreg"
-  "${CMAKE_CURRENT_LIST_DIR}/../../ossim-registration-source")
+  "${CMAKE_CURRENT_LIST_DIR}/../../ossim-autoreg")
+
+find_package(ossim-autoreg CONFIG QUIET
+  HINTS ${_OSSIM_REGISTRATION_SOURCE_HINTS}
+  PATH_SUFFIXES lib/cmake/ossim-autoreg)
+
+if(TARGET ossim_registration_source::ossim-registration-source)
+  set(ossim-registration-source_FOUND TRUE)
+  return()
+endif()
+
+if(TARGET ossim_autoreg::ossim-registration-source)
+  add_library(ossim_registration_source::ossim-registration-source INTERFACE IMPORTED)
+  set_target_properties(ossim_registration_source::ossim-registration-source PROPERTIES
+    INTERFACE_LINK_LIBRARIES ossim_autoreg::ossim-registration-source)
+  set(ossim-registration-source_FOUND TRUE)
+  return()
+endif()
 
 find_path(OSSIM_REGISTRATION_SOURCE_INCLUDE_DIR
   NAMES ossim/registration/ossimFixedRegistrationSource.h
